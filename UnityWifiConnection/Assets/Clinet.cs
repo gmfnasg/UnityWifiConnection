68fd4b1c122115d053e0eefc8be667ba93a6034d
gmfnasg <gpx_mark_farng@hotamil.com>
2015-12-27 11:23:38 +0800

    Basic

---
+++ UnityWifiConnection/Assets/Clinet.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clinet : MonoBehaviour {
+	public bool showDetail;
+	public bool connect = false;
+
+	void Awake(){
+		if (!gameObject.GetComponent<NetworkView> ()) {
+			gameObject.AddComponent<NetworkView>();
+		}
+	}
+
+	void Update () {
+		MasterServer.RequestHostList("YourPCRoom");
+	}
+
+	void OnGUI(){
+		GUILayout.Label ("");
+		GUILayout.Label ("");
+		GUILayout.Label ("");
+		if(GUILayout.Button("showDetail")){
+			showDetail = !showDetail;
+		}
+		HostData[] datas = MasterServer.PollHostList ();
+
+		if (!showDetail)
+			return;
+
+		for (int i=0; i<datas.Length; i++) {
+			HostData data = datas[i];
+			if(data==null)
+				continue;
+
+			GUILayout.BeginHorizontal();
+			Debug.Log("gameName("+data.gameName+")"
+			          +", connectedPlayers("+data.connectedPlayers+"/"+data.playerLimit+")");
+
+
+			string logInfo = "";
+			for(int j=0;j<data.ip.Length;j++){
+				string host = data.ip[j];
+				logInfo += " [Host("+host+") port("+data.port+")] ";
+			}
+			Debug.Log(logInfo);
+
+			Debug.Log("comment("+data.comment+")");
+			if(GUILayout.Button("Connect to server")){
+				Network.Connect(data);
+				connect = true;
+			}
+			GUILayout.EndHorizontal();
+		}
+		if (connect) {
+			if(GUILayout.Button("+")){
+				if (GetComponent<NetworkView> ()) {
+					GetComponent<NetworkView> ().RPC("Add", RPCMode.All, 1);
+				}
+			}
+			if(GUILayout.Button("+")){
+				if (GetComponent<NetworkView> ()) {
+					GetComponent<NetworkView> ().RPC("Subtract", RPCMode.All, 1);
+				}
+			}
+		}
+	}
+
+}
---
+++ UnityWifiConnection/Assets/Server.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Server : MonoBehaviour {
+	public Rect serverConnectionsRect = new Rect();
+	public int amount = 0;
+	
+	void Awake(){
+		if (!GetComponent<NetworkView> ()) {
+			gameObject.AddComponent<NetworkView>();
+		}
+
+	}
+
+	void OnGUI(){
+		if (GUILayout.Button ("StartServer")) {
+			Network.InitializeServer(99, 9998, !Network.HavePublicAddress()); 
+			MasterServer.RegisterHost("YourPCRoom", "PhantasyNan", "alpha 1.0");
+			GUI.Label(serverConnectionsRect, "Connections:"+Network.connections.Length);
+		}
+		GUILayout.Label ("amount:"+amount.ToString());
+	}
+
+	[RPC]
+	void Add(int add){
+		Debug.Log ("Do Add");
+		amount += add;
+	}
+
+	[RPC]
+	void Subtract(int subtract){
+		Debug.Log ("Do Subtract");
+		amount -= subtract;
+	}
+}
